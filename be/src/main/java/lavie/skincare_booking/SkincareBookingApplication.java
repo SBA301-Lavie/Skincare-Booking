3c233df0717e4d8d136b0f5806f948ce7cd2bee6
doanvinhphu <doanvinhphu321@gmail.com>
2025-01-12 13:04:15 +0700

    Initial Backend Source

---
+++ be/src/main/java/lavie/skincare_booking/SkincareBookingApplication.java
@@ -0,0 +1,13 @@
+package lavie.skincare_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkincareBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkincareBookingApplication.class, args);
+	}
+
+}
